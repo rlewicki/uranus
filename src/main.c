9f4d199bd347051818c3713cbc238ff848d1bd7d
rlewicki <zguba44@gmail.com>
2019-03-05 22:50:27 +0100

    Showing first spirte on screen

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <gb/gb.h>
+#include "sprites/character.c"
 
-int main()
+int main(void)
 {
-    printf("hello world\n");
+    set_sprite_data(0, 1, character_sprite);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 50, 50);
+    SHOW_SPRITES;
     return 0;
 }
