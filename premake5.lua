8c8be0b61444c6ccbe4576da7f9cda6bd224300a
rlewicki <zguba44@gmail.com>
2019-03-05 21:33:35 +0100

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,18 @@
+workspace "uranus-gba"
+    configurations { "Debug", "Release"}
+
+project "uranus-gba"
+   kind "GameBoyApp"
+   language "C"
+   targetdir "bin/%{cfg.buildcfg}"
+   linkoptions { "-Wa-l", "-Wl-m", "-Wl-j" }
+
+   files { "**.h", "**.c" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
