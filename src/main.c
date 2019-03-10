53a11a4376ac66cd7ec07e576c3ce13c22707801
rlewicki <zguba44@gmail.com>
2019-03-10 16:49:54 +0100

    Changing sprite tile based on current direction

--- src/main.c
+++ src/main.c
@@ -25,8 +25,12 @@ const BYTE START_POS_Y = 8 * 13;
 #define PLAYER_SPRITE_INDEX 0
 #define PLAYER_MOVEMENT_SPEED 1
 #define PLAYER_SPRITE sprites_container[PLAYER_SPRITE_INDEX]
-#define PLAYER_IDLE_TILE_INDEX 1
-#define ANIMATION_STEP_DURATION 5
+#define PLAYER_IDLE_TILE_INDEX 0
+#define PLAYER_LEFT_TILE_INDEX 1
+#define PLAYER_RIGHT_TILE_INDEX 2
+#define PLAYER_BOTTOM_TILE_INDEX 3
+#define PLAYER_TOP_TILE_INDEX 4
+#define ANIMATION_STEP_DURATION 8
 
 struct Sprite sprites_container[MAX_SPRITE_COUNT];
 BYTE next_sprite_index;
@@ -52,8 +56,8 @@ void init_map(void)
 
 void init_player_sprite(void)
 {
-    set_sprite_data(PLAYER_SPRITE_INDEX, 2, pacman_tiles);
-    set_sprite_tile(PLAYER_SPRITE_INDEX, 0);
+    set_sprite_data(PLAYER_SPRITE_INDEX, 5, pacman_tiles);
+    set_sprite_tile(PLAYER_SPRITE_INDEX, PLAYER_IDLE_TILE_INDEX);
     move_sprite(PLAYER_SPRITE_INDEX, START_POS_X, START_POS_Y);
 
     PLAYER_SPRITE.id = PLAYER_SPRITE_INDEX;
@@ -89,18 +93,22 @@ void process_input(void)
     if(joypad() & J_RIGHT)
     {
         current_direction = DIR_RIGHT;
+        current_direction_tile_index = PLAYER_RIGHT_TILE_INDEX;
     }
     else if(joypad() & J_LEFT)
     {
         current_direction = DIR_LEFT;
+        current_direction_tile_index = PLAYER_LEFT_TILE_INDEX;
     }
     else if(joypad() & J_UP)
     {
         current_direction = DIR_TOP;
+        current_direction_tile_index = PLAYER_TOP_TILE_INDEX;
     }
     else if(joypad() & J_DOWN)
     {
         current_direction = DIR_BOTTOM;
+        current_direction_tile_index = PLAYER_BOTTOM_TILE_INDEX;
     }
 }
 
@@ -140,7 +148,7 @@ void animate_player(void)
         
         set_sprite_tile(PLAYER_SPRITE_INDEX, current_animation_tile_index);
     }
-    
+
     animation_frame_count++;
 }
 
